3c087c0f8ccebcc292911db1c109463fa21e401c
Jaemilton Oliveira <jaemiltonro@yahoo.com.br>
2026-02-21 12:12:18 -0300

    Add modelo.env configuration file and update prepare.bat to copy it to .env

--- prepare.bat
+++ prepare.bat
@@ -1,2 +1,6 @@
 python -m venv venv
 .\venv\Scripts\pip.exe install -r .\requirements.txt
+# check if .env file exists, if not copy modelo.env to .env
+if not exist .env (
+    copy modelo.env .env
+)
